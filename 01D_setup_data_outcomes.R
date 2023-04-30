f6a75df07abc002dcd440e28b7a13d3734412944
Tyler Smith <tylersmith@jhu.edu>
2023-04-30 10:05:48 -0700

    add header

---
+++ 01D_setup_data_outcomes.R
@@ -0,0 +1,10 @@
+################################################################################
+# Pregnancy, Arsenic, and Immune Response (PAIR) Study
+# Prenatal Arsenic and Acute Morbidity -- Data - Outcomes
+
+# Tyler Smith
+# April 28, 2023
+
+##### Preliminaries ############################################################
+# Load Packages
+library(tidyverse)
