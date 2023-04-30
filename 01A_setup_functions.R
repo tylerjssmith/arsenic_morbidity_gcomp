0b048f9965821f11a46b04f72733880c7bfd8094
Tyler Smith <tylersmith@jhu.edu>
2023-04-30 07:53:45 -0700

    add p-value function

--- 01A_setup_functions.R
+++ 01A_setup_functions.R
@@ -30,3 +30,24 @@ check_discrete <- function(data, x)
     count({{ x }}) %>%
     mutate(p = n / sum(n) * 100)
 }
+
+##### Functions: Tables ########################################################
+# Function: p-values for Tables 1-2
+tbl_pval <- function(x, ...) {
+  y <- unlist(x)
+  g <- factor(rep(1:length(x), times=sapply(x, length)))
+    
+  if (is.numeric(y)) {
+    # Continuous: One-way ANOVA
+    p <- anova(lm(y ~ g))[1,5]
+  } else {
+    # Categorical: Chi-square Test
+    p <- chisq.test(table(y, g))$p.value
+  }
+    
+  c("", sub("<", "&lt;", format.pval(p, digits=3, eps=0.001)))
+}
+
+##### Functions: Figures #########################################################
+# Function: Base 10
+base10 <- function(x) 10 ^ x
